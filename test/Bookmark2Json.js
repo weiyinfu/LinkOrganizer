6bdc5579f98891fd11405d4e866f16a89e76fecc
wyf <wei.yinfu@qq.com>
2019-01-30 13:41:53 +0800

    good

--- test/Bookmark2Json.js
+++ test/Bookmark2Json.js
@@ -1,6 +1,6 @@
 const bookmark2json = require("../vues/js/BookmarkJson").bookmark2json
 const path = require("path")
 const fs = require("fs")
-var a = bookmark2json(path.join(__dirname, "bookmark.html"))
+var a = bookmark2json(path.join(__dirname, "chrome-bookmark.html"))
 console.log(a)
 fs.writeFileSync(path.join(__dirname, "bookmark.json"), JSON.stringify(a))
